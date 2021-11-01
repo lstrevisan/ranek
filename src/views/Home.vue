5ce7443610a8a3a3fe7a87feb98aeea1ff462140
lstrevisan <lstrevisan@gmail.com>
2021-11-01 16:25:26 -0300

    Segundo commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-  </div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue"
+import ProdutosLista from "@/components/ProdutosLista.vue"
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    ProdutosBuscar, ProdutosLista
+  }
 }
 </script>
+<style scoped>
+  h1{
+    font-size: 2rem;
+    color: #87f;
+    margin-top: 40px;
+    text-align: center;
+  }
+</style>
